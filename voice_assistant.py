1fbc43f6f85fe78d0302213b73fc3af41ee36bcf
Avish Shah <avishshah1108@gmail.com>
2020-08-14 16:11:24 +0530

    speech recognition starting

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import os
+import speech_recognition as sr
+import sys
+
+
+def listen():
+    mic = sr.Microphone()
+    with mic as source:
+        audio = r.listen(source)
+        r.recognize_google(audio)
+       
