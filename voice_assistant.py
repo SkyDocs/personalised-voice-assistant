4e0ca05c08cd4232587c21ef7cd25230b7d753b5
Harshit Ruwali <harshitruwali@outlook.com>
2020-08-14 20:54:29 +0530

    Fixed webbrowser function

--- voice_assistant.py
+++ voice_assistant.py
@@ -4,25 +4,26 @@ import speech_recognition as sr
 import webbrowser
 
 def listen():
-    mic = sr.Microphone()
-    r = sr.Recognizer()
-    with mic as source:
-        audio = r.listen(source)
+	mic = sr.Microphone()
+	r = sr.Recognizer()
+	with mic as source:
+		audio = r.listen(source)
 
-    try:
-    	command = r.recognize_google(audio).lower()
-    	print("You said : " + command)
+	try:
+		command = r.recognize_google(audio).lower()
+		print("You said : " + command)
+		# main(command)
 
-    except sr.UnknownValueError:
-    	print("Error occured, try again")
-    	command = listen()
+	except sr.UnknownValueError:
+		print("Error occured, try again")
+		command = listen()
 
-    return command
-       
+	return command
+	   
+	
+def main(command):
+	if "open" in command:
+		webbrowser.open("https://www.google.com")
 
 
-	def main(command):
-		if open in command:
-			webbrowser.open(google.com)
-
-listen()
+main(listen())
