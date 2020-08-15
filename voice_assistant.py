bb6f5b94cdb5fb6c6b653acbf11c0fe8e3311a7e
Avish Shah <avishshah1108@gmail.com>
2020-08-15 17:37:11 +0530

    add wake word

--- voice_assistant.py
+++ voice_assistant.py
@@ -3,28 +3,24 @@ import sys
 import speech_recognition as sr
 import webbrowser
 import datetime
-# from gtts import gTTS
-# import playsound
 import pyttsx3
+from selenium import webdriver
 
 def bot(talk):
 	print(talk)
-	# text_to_speech = gTTS(text=talk, lang='en', slow=False)
-	# x = text_to_speech.save('sound.mp3')
-	# playsound.playsound(x, True)
+	
 	engine = pyttsx3.init()
 	sound = engine.getProperty('voices')
 
 	for i in str(talk).splitlines():
 		engine.say(talk)
 	engine.runAndWait()
-	
 
 def listen():
 	mic = sr.Microphone()
 	r = sr.Recognizer()
 	with mic as source:
-		audio = r.listen(source, phrase_time_limit= 5)
+		audio = r.listen(source, phrase_time_limit=5)
 	
 	try:
 		command = r.recognize_google(audio).lower()
@@ -35,17 +31,23 @@ def listen():
 		print("Error occured, try again")
 		command = listen()
 
-	return command
-	   
+	return command	   
 	
 def main(command):
-	if "open" in command:
+	if "open google" in command:
 		webbrowser.open("https://www.google.com")
 		#print("webbrowser opened")
 		bot("Your web browser is opened! ")
-	elif "time" in command:
+	elif "current time" in command:
 		current = datetime.datetime.now()
 		print(current)
+	elif "open youtube" in command:
+		browser = webdriver.Firefox()
+		bot("opening youtube")
+		url = "https://www.youtube.com"
+		browser.get(url)
+
+
 
 
 
