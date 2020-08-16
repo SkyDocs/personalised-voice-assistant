05b017fddf17b3d162f9d93f27f3a259f1cb28e4
Avish Shah <avishshah1108@gmail.com>
2020-08-16 16:32:25 +0530

    wake word testing

--- voice_assistant.py
+++ voice_assistant.py
@@ -2,14 +2,21 @@ import os
 import sys
 import speech_recognition as sr
 import webbrowser
+import time
+import pyjokes
+import subprocess
 import datetime
 import pyttsx3
-from selenium import webdriver
+
 
 def bot(talk):
 	print(talk)
-	
+
 	engine = pyttsx3.init()
+	rate = engine.getProperty('rate')
+	engine.setProperty('rate', 180)
+	volume = engine.getProperty('volume')
+	engine.setProperty('volume', 1.0)
 	sound = engine.getProperty('voices')
 
 	for i in str(talk).splitlines():
@@ -20,7 +27,8 @@ def listen():
 	mic = sr.Microphone()
 	r = sr.Recognizer()
 	with mic as source:
-		audio = r.listen(source, phrase_time_limit=5)
+		print("say something")
+		audio = r.listen(source)
 	
 	try:
 		command = r.recognize_google(audio).lower()
@@ -29,23 +37,61 @@ def listen():
 	
 	except sr.UnknownValueError:
 		print("Error occured, try again")
+		#bot("Sorry I did not get that. Please try again.")
 		command = listen()
 
-	return command	   
+	return command	  
+
+def activate(talk):
+	wake_words = {'ok siri'}
+
+	talk = talk.lower()
+	for phrase in wake_words:
+		if phrase in talk:
+			return True
+	return False  
 	
 def main(command):
-	if "open google" in command:
+	if "hello" in command:
+		bot("Hey, this is your voice assistant, developed by a team at skydocs.")
+	elif "joke" in command:
+		bot(pyjokes.get_joke())		
+	elif "open google" in command:
 		webbrowser.open("https://www.google.com")
 		#print("webbrowser opened")
 		bot("Your web browser is opened! ")
 	elif "current time" in command:
 		current = datetime.datetime.now()
 		print(current)
-	elif "open youtube" in command:
-		browser = webdriver.Firefox()
+	elif "youtube" in command:
 		bot("opening youtube")
 		url = "https://www.youtube.com"
-		browser.get(url)
+		webbrowser.open(url)
+	elif "gmail" in command:
+		bot("sure, opening gmail")
+		url_mail = "https://www.gmail.com"
+		webbrowser.open(url_mail)
+	elif "wikipedia" in command:
+		url_wiki = "https://www.wikipedia.org/"
+		webbrowser.open(url_wiki)
+		bot("Sure! Here you go.")
+	elif "lock screen" in command:
+		bot("Locking your screen")
+		os.popen('gnome-screensaver-command --lock')
+	elif "news" in command:
+		bot("opening google news")
+		news_url = "news.google.com"
+		webbrowser.open(news_url)
+	elif "map" in command:
+		bot("opening maps powered by google")
+		maps_url = "google.com/maps"
+		webbrowser.open(maps_url)
+	elif "shutdown" in command:
+		bot("Shutting down your system")
+		os.system("poweroff")
+	elif "reminder" in command:
+		bot("What shall I remind you about ?")
+	
 
 
 
